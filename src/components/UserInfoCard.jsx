67ad6c8568741f78778428ec91dc699e15690121
Yiğit Kılınçcıoğlu <yigit22167@gmail.com>
2025-04-23 16:14:55 +0300

    css adjustements

--- src/components/UserInfoCard.jsx
+++ src/components/UserInfoCard.jsx
@@ -4,44 +4,45 @@ import { FaLocationDot } from "react-icons/fa6";
 
 const UserInfoCard = ({ userData }) => {
   return (
-    <div className="flex flex-col sm:flex-row justify-center gap-x-9 items-center">
-      <div className="flex items-center justify-center">
-        <div
-          className={`${userData.name || userData.bio ? "md:mr-6 mr-4" : ""}`}
-        >
+    <div className="flex flex-col w-full max-w-2xl p-4 sm:flex-row justify-center gap-x-9 items-center">
+      {/* User Info Section */}
+      <div className="flex flex-col sm:flex-row justify-center items-center">
+        <div className="">
           <img
-            className="shrink-0 rounded-full w-32"
+            className="rounded-full mx-auto sm:mr-6 sm:mb-0 mb-4 min-w-32 max-w-32"
             src={userData.avatar_url}
             alt={userData.name}
           />
         </div>
-        <div className="flex flex-col gap-y-1">
-          <h2 className="text-xl mb-1 md:text-2xl text-slate-100">
-            {userData.name}
-          </h2>
+        <div className="flex flex-col sm:items-baseline items-center justify-center gap-y-1">
+          <h2 className="text-2xl text-slate-100">{userData.name}</h2>
           {userData.bio && (
-            <p className="text-lg md:text-xl text-slate-100">{userData.bio}</p>
+            <p className="text-xl sm:text-start text-center text-slate-100">
+              {userData.bio}
+            </p>
           )}
           {userData.email && (
-            <p className="text-lg md:text-xl flex items-center text-slate-100">
+            <p className="text-xl flex items-center text-slate-100">
               <MdEmail className="mr-1.5" />
-              {user.email}
+              {userData.email}
             </p>
           )}
           {userData.location && (
-            <p className="text-lg md:text-xl flex items-center text-slate-100">
+            <p className="text-xl flex items-center text-slate-100">
               <FaLocationDot className="mr-1.5" />
               {userData.location}
             </p>
           )}
           {userData.company && (
-            <p className="text-lg md:text-xl flex items-center text-slate-100">
+            <p className="text-xl flex items-center text-slate-100">
               <FaBuilding className="mr-1.5" />
-              {user.company}
+              {userData.company}
             </p>
           )}
         </div>
       </div>
+
+      {/* Follow button */}
       <a
         className="text-slate-100 font-bold bg-indigo-800 cursor-pointer text-xl sm:self-start self-center mt-6 sm:mt-1.5 py-2.5 px-6 rounded-lg hover:bg-violet-700 transition-all"
         href={userData.html_url}
